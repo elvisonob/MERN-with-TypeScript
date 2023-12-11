3bcb407c56099ab045ffa8ad50743ba20d74721f
elvisonob <elvisonob@yahoo.com>
2023-12-11 05:02:42 +0000

    Union types added

--- src/state/GuestList.tsx
+++ src/state/GuestList.tsx
@@ -11,13 +11,13 @@ const GuestList: React.FC = () => {
   return (
     <div>
       <h3>Guest List</h3>
-      <input value={name} onChange={(e) => setName(e.target.value)} />
-      <button onClick={onClick}>Add Guest</button>
       <ul>
         {guests.map((guest) => (
-          <li>{guest}</li>
+          <li key={guest}>{guest}</li>
         ))}
       </ul>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={onClick}>Add Guest</button>
     </div>
   );
 };
